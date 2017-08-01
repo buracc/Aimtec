302b888086db110eaf1b645ccebaa753d77bc14a
karos1 <burakinan1@hotmail.com>
2017-08-01 23:00:28 +0200

    test

--- Sivir/Sivir/Sivir.cs
+++ Sivir/Sivir/Sivir.cs
@@ -5,6 +5,17 @@ using Aimtec.SDK.Menu.Config;
 using Aimtec.SDK.TargetSelector;
 using System.Linq;
 
+using Aimtec;
+using Aimtec.SDK.Damage;
+using Aimtec.SDK.Extensions;
+using Aimtec.SDK.Menu;
+using Aimtec.SDK.Menu.Components;
+using Aimtec.SDK.Orbwalking;
+using Aimtec.SDK.TargetSelector;
+using Aimtec.SDK.Util.Cache;
+using Aimtec.SDK.Prediction.Skillshots;
+using Aimtec.SDK.Util;
+
 namespace Sivir
 {
     internal partial class Sivir
@@ -16,9 +27,34 @@ namespace Sivir
             InitMethods();
         }
 
+
+        public static Obj_AI_Hero GetBestEnemyHeroTarget()
+        {
+            return GetBestEnemyHeroTargetInRange(float.MaxValue);
+        }
+
+        public static Obj_AI_Hero GetBestEnemyHeroTargetInRange(float range)
+        {
+            var ts = TargetSelector.Implementation;
+            var target = ts.GetTarget(range);
+            if (target != null && target.IsValidTarget())
+            {
+                return target;
+            }
+
+            var firstTarget = ts.GetOrderedTargets(range)
+                .FirstOrDefault(t => t.IsValidTarget());
+            if (firstTarget != null)
+            {
+                return firstTarget;
+            }
+
+            return null;
+        }
+
         public void OnTick()
         {
-            target = TargetSelector.GetTarget(1500);
+            target = GetBestEnemyHeroTargetInRange(1500);
 
             if (GlobalKeys.ComboKey.Active)
             {
